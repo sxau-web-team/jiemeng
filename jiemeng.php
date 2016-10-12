8aa3668121f3f077d2d382c02ff42520b7f5c00b
dubuqingfeng <1135326346@qq.com>
2016-10-12 23:18:08 +0800

    500?

--- jiemeng.php
+++ jiemeng.php
@@ -14,6 +14,8 @@
  
 header('Content-type:application/json;charset=utf-8');
 header('Access-Control-Allow-Origin：*');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Max-Age: 86400');    // cache for 1 day
  
 //配置您申请的appkey
 $appkey = "00f9ff1293e505d93e9b2571c4b16ee4"; 
