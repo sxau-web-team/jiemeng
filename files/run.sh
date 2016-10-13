d50906f72c489de0f1e5895b4633867edd81b782
dubuqingfeng <1135326346@qq.com>
2016-10-13 09:42:13 +0800

    update Dockerfile

---
+++ files/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+[ -f /run-pre.sh ] && /run-pre.sh
+
+if [ ! -d /data/htdocs ] ; then
+  mkdir -p /data/htdocs
+  chown :www-data /data/htdocs
+fi
+
+# start php-fpm
+mkdir -p /data/logs/php-fpm
+php-fpm
+
+# start nginx
+mkdir -p /data/logs/nginx
+mkdir -p /data/logs/php-fpm
+mkdir -p /tmp/nginx
+chown nginx /tmp/nginx
+nginx
